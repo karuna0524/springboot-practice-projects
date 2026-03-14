f2e49d3c49189a82fd1d6eb0f406d8c0681bf809
karuna <karunakuriyaar2004@gmail.com>
2026-03-14 23:32:01 +0530

    a simple html view project using springboot added

---
+++ ProjectWithView/src/main/java/com/example/demo/ProjectWithViewApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWithViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWithViewApplication.class, args);
+	}
+
+}
